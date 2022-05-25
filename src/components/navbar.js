4f431628673fbd5f6255345b4be0f571f5eafd20
BrianSammit <briansammit.cruz@gmail.com>
2022-05-24 19:01:45 -0500

    Add navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,10 @@
+import { GiEarthAmerica } from 'react-icons/gi';
+import './navbar.css'
+
+export default function Navbar () {
+ return (
+    <nav>
+      <h1><span className='navbar--earth_icon'><GiEarthAmerica /></span> My travel journal</h1>
+    </nav>
+  )
+}
