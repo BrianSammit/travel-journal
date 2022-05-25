c04367327689fb66987eba2c235b92fa3df83520
BrianSammit <briansammit.cruz@gmail.com>
2022-05-24 22:32:11 -0500

    Fix data and image size

--- src/components/data.js
+++ src/components/data.js
@@ -11,22 +11,22 @@ export default [
   },
   {
     id: 2,
-    title: "London Eye", 
-    location: "London",
-    googleMapsUrl: "https://www.google.com/maps/place/lastminute.com+London+Eye/@51.5032973,-0.1195537,15z/data=!4m5!3m4!1s0x0:0x4291f3172409ea92!8m2!3d51.5032973!4d-0.1195537",
-    startDate: "10 August 2018",
-    endDate: "15 August 2018",
+    title: "Cartagena", 
+    location: "Colombia",
+    googleMapsUrl: "https://www.google.com/maps/place/Cartagena,+Cartagena+Province,+Bolivar/@10.4001987,-75.5085254,13z/data=!3m1!4b1!4m5!3m4!1s0x8ef625e7ae9d1351:0xb161392e033f26ca!8m2!3d10.3932277!4d-75.4832311",
+    startDate: "20 February 2022",
+    endDate: "26 February 2022",
     description: "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident.",
-    imageUrl: "https://res.cloudinary.com/hello-tickets/image/upload/c_limit,f_auto,q_auto,w_1300/v1644633239/q13q90jc6acltbqnvhky.jpg"
+    imageUrl: "https://denomades.s3.us-west-2.amazonaws.com/blog/wp-content/uploads/2020/05/11141948/ca-times.brightspotcdn.com_.jpg"
   },
   {
     id: 3,
-    title: "London Eye", 
-    location: "London",
+    title: "Panama", 
+    location: "Panama",
     googleMapsUrl: "https://www.google.com/maps/place/lastminute.com+London+Eye/@51.5032973,-0.1195537,15z/data=!4m5!3m4!1s0x0:0x4291f3172409ea92!8m2!3d51.5032973!4d-0.1195537",
-    startDate: "10 August 2018",
-    endDate: "15 August 2018",
+    startDate: "10 July 2015",
+    endDate: "15 July 2015",
     description: "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident.", 
-    imageUrl: "https://res.cloudinary.com/hello-tickets/image/upload/c_limit,f_auto,q_auto,w_1300/v1644633239/q13q90jc6acltbqnvhky.jpg"
+    imageUrl: "http://blog-dialogoafondo.imf.org/wp-content/uploads/2019/05/Dialogo-1024x600-panama-slphotography-gettyimages-iStock-931040902-1-1024x600.jpg"
   },
 ] 
